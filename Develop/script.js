1e7722aa8b43bad9da7d28fe933d8991e6155fad
Erin V <erinkvoelker@gmail.com>
2022-06-24 22:02:29 -0400

    fixed issue in time-block task name, managed to get function to save to local storage and pull relevant data

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,58 @@
-    // sets the current day & time in the currentDay html element using moment.js
- var todaysDate = moment().format("dddd, MMMM Do, YYYY");
-$("#currentDay").html(todaysDate);
+// sets the current day & time in the currentDay html element using moment.js
+$(document).ready(function () {
 
-function hourColorCode() {
+    var todaysDate = moment().format("dddd, MMMM Do, YYYY");
+    $("#currentDay").html(todaysDate);
+    var currentHour = moment().format("HH");
+
+    function hourColorCode() {
         // setting the current hour in moment.js to set up past, present, future hour color coordination
-        var currentHour = moment().format("HH");
         $(".time-block").each(function() {
-            var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
             
-            if (hourBlock < currentHour) {
-                $(this).addClass("past");
-                $(this).removeClass("present");
-                $(this).removeClass("future");
-            } else if (hourBlock === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            }
-        });
-}
+        if (hourBlock < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        } else if (hourBlock === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+    }
 
-$(".saveBtn").click(function(event) {
-    event.preventDefault();
-    var task = $(this).siblings(".time-block").val();
-    var time = $(this).parent("id").val();
-    localStorage.setItem(time, task);
+    $(".saveBtn").click(function(event) {
+        event.preventDefault();
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id").split("-")[1];
+        localStorage.setItem(time, value);
+    });
+
+    $(".clear-button").click(function(event){
+        event.preventDefault();
+        $("textarea").val("");
+        localStorage.clear();
+    });
+
+    $("#hour-8 .description").val(localStorage.getItem("8"));
+    $("#hour-9 .description").val(localStorage.getItem("9"));
+    $("#hour-10 .description").val(localStorage.getItem("10"));
+    $("#hour-11 .description").val(localStorage.getItem("11"));
+    $("#hour-12 .description").val(localStorage.getItem("12"));
+    $("#hour-13 .description").val(localStorage.getItem("13"));
+    $("#hour-14 .description").val(localStorage.getItem("14"));
+    $("#hour-15 .description").val(localStorage.getItem("15"));
+    $("#hour-16 .description").val(localStorage.getItem("16"));
+    $("#hour-17 .description").val(localStorage.getItem("17"));
+
+    hourColorCode();
+
 });
-
-$("#8 .task-entry").val(localStorage.getItem("8"))
-$("#9 .task-entry").val(localStorage.getItem("9"))
-$("#10 .task-entry").val(localStorage.getItem("10"))
-$("#11 .task-entry").val(localStorage.getItem("11"))
-$("#12 .task-entry").val(localStorage.getItem("12"))
-$("#13 .task-entry").val(localStorage.getItem("13"))
-$("#14 .task-entry").val(localStorage.getItem("14"))
-$("#15 .task-entry").val(localStorage.getItem("15"))
-$("#16 .task-entry").val(localStorage.getItem("16"))
-$("#17 .task-entry").val(localStorage.getItem("17"))
-
-hourColorCode();
-
-
 
 
 
